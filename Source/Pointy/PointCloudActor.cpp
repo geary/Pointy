aeb3331cb10ee806733f8169e33504d457c563b1
Michael Geary <mg@mg.to>
2016-05-15 02:06:04 -0700

    Remove extra one

--- Source/Pointy/PointCloudActor.cpp
+++ Source/Pointy/PointCloudActor.cpp
@@ -2,55 +2,25 @@
 #include "PointCloudActor.h"
 
 
-// Sets default values
 APointCloudActor::APointCloudActor() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//USphereComponent* sphereComponent = CreateDefaultSubobject<USphereComponent>( TEXT("RootComponent") );
-	//RootComponent = sphereComponent;
-	//sphereComponent->InitSphereRadius( 40.0f );
-	//sphereComponent->SetCollisionProfileName( "Pawn" );
-
-	//UStaticMeshComponent* sphereMesh = CreateDefaultSubobject<UStaticMeshComponent>( TEXT("VisualRepresentation") );
-	//sphereMesh->AttachTo( RootComponent );
-	//static ConstructorHelpers::FObjectFinder<UStaticMesh> sphereMeshAsset( TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere") );
-	//if( sphereMeshAsset.Succeeded() ) {
-	//	sphereMesh->SetStaticMesh( sphereMeshAsset.Object );
-	//	sphereMesh->SetRelativeLocation( FVector::ZeroVector );
-	//	sphereMesh->SetWorldScale3D( FVector(0.8f) );
-	//}
-
 	pointicles = CreateDefaultSubobject<UParticleSystemComponent>( TEXT("Pointicles") );
 
-	//pointicles->AttachTo( sphereMesh );
 	pointicles->AttachTo( RootComponent );
-	//pointicles->bAutoActivate = true;
-	//pointicles->SetRelativeLocation( FVector(0.0f, 0.0f, 0.0f) );
+	pointicles->bAutoActivate = false;
 
 	static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/P_Fire.P_Fire") );
 	if( particleAsset.Succeeded() ) {
 		pointicleSystem = particleAsset.Object;
 		pointicles->SetTemplate( pointicleSystem );
 	}
-
-	//sphereComponent->SetSimulatePhysics(true);
-
-	//pointicles = CreateDefaultSubobject<UParticleSystemComponent>( TEXT("Pointicles") );
-	//pointicles->AttachTo( RootComponent );
-
-	//RootComponent = pointicles;
-	//auto b = pointicles->bAutoActivate;
-	//pointicles->bAutoActivate = true;
-	//pointicles->SetRelativeLocation( FVector::ZeroVector );
 }
 
-// Called when the game starts or when spawned
 void APointCloudActor::BeginPlay() {
 	Super::BeginPlay();
 	
-	//pointicles->Activate( true );
-
 	auto point = GetActorLocation();
 	auto count = 3, distance = 50;
 
@@ -67,7 +37,6 @@ void APointCloudActor::BeginPlay() {
 	}
 }
 
-// Called every frame
 void APointCloudActor::Tick( float DeltaTime ) {
 	Super::Tick( DeltaTime );
 }
--- Source/Pointy/PointCloudActor.h
+++ Source/Pointy/PointCloudActor.h
@@ -14,13 +14,10 @@ public:
 	UParticleSystem* pointicleSystem;
 	UParticleSystemComponent* pointicles;
 
-	// Sets default values for this actor's properties
 	APointCloudActor();
 
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
-	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 
 private:
