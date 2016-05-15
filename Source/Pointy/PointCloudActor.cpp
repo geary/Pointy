9ef9f4e8f5b72021183d8c7a586eb4c0a08d9c13
Michael Geary <mg@mg.to>
2016-05-15 00:44:56 -0700

    Single point particle system

--- Source/Pointy/PointCloudActor.cpp
+++ Source/Pointy/PointCloudActor.cpp
@@ -7,27 +7,31 @@ APointCloudActor::APointCloudActor() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	USphereComponent* sphereComponent = CreateDefaultSubobject<USphereComponent>( TEXT("RootComponent") );
-	RootComponent = sphereComponent;
-	sphereComponent->InitSphereRadius( 40.0f );
-	sphereComponent->SetCollisionProfileName( "Pawn" );
+	//USphereComponent* sphereComponent = CreateDefaultSubobject<USphereComponent>( TEXT("RootComponent") );
+	//RootComponent = sphereComponent;
+	//sphereComponent->InitSphereRadius( 40.0f );
+	//sphereComponent->SetCollisionProfileName( "Pawn" );
 
-	UStaticMeshComponent* sphereMesh = CreateDefaultSubobject<UStaticMeshComponent>( TEXT("VisualRepresentation") );
-	sphereMesh->AttachTo( RootComponent );
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> sphereMeshAsset( TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere") );
-	if( sphereMeshAsset.Succeeded() ) {
-		sphereMesh->SetStaticMesh( sphereMeshAsset.Object );
-		sphereMesh->SetRelativeLocation( FVector::ZeroVector );
-		sphereMesh->SetWorldScale3D( FVector(0.8f) );
-	}
+	//UStaticMeshComponent* sphereMesh = CreateDefaultSubobject<UStaticMeshComponent>( TEXT("VisualRepresentation") );
+	//sphereMesh->AttachTo( RootComponent );
+	//static ConstructorHelpers::FObjectFinder<UStaticMesh> sphereMeshAsset( TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere") );
+	//if( sphereMeshAsset.Succeeded() ) {
+	//	sphereMesh->SetStaticMesh( sphereMeshAsset.Object );
+	//	sphereMesh->SetRelativeLocation( FVector::ZeroVector );
+	//	sphereMesh->SetWorldScale3D( FVector(0.8f) );
+	//}
 
 	pointicles = CreateDefaultSubobject<UParticleSystemComponent>( TEXT("Pointicles") );
 
-	pointicles->AttachTo( sphereMesh );
-	pointicles->bAutoActivate = true;
-	pointicles->SetRelativeLocation( FVector(0.0f, 0.0f, 0.0f) );
+	//pointicles->AttachTo( sphereMesh );
+	pointicles->AttachTo( RootComponent );
+	//pointicles->bAutoActivate = true;
+	//pointicles->SetRelativeLocation( FVector(0.0f, 0.0f, 0.0f) );
 
-	static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire") );
+	//static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/StarterContent/Particles/P_Fire.P_Fire") );
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/P_Fire.P_Fire") );
+	//static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/StarterContent/Particles/P_Sparks.P_Sparks") );
+	//static ConstructorHelpers::FObjectFinder<UParticleSystem> particleAsset( TEXT("/Game/Pointy.Pointy") );
 	if( particleAsset.Succeeded() ) {
 		pointicles->SetTemplate( particleAsset.Object );
 	}
