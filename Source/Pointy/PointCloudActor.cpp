5eb70b37601e16c289fb16a880fc46e2864b2d55
Michael Geary <mg@mg.to>
2016-05-27 23:29:02 -0700

    Turn off bCanEverTick

--- Source/Pointy/PointCloudActor.cpp
+++ Source/Pointy/PointCloudActor.cpp
@@ -4,7 +4,7 @@
 
 APointCloudActor::APointCloudActor() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	pointicles = CreateDefaultSubobject<UParticleSystemComponent>( TEXT("Pointicles") );
 
